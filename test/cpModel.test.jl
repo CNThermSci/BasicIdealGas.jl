a9020688aefbf340f7c1c14adb44391dece45bb8
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-07-01 13:20:08 -0300

    UPD All 24 tests passing

--- test/cpModel.test.jl
+++ test/cpModel.test.jl
@@ -13,21 +13,39 @@ end
 
 @testset "cpModel.test.jl: inner constructor return types                           " begin
     for ℙ in union2vec(Base.IEEEFloat)
-        ID, B = :CO2_CB7_test, :MO
-        CP = T -> ℙ(22.26 + 5.891e-2 * T - 3.501e-5 * T^2 + 7.469e-9 * T^3)
-        Tmin, Tmax, Tref  = 273, 1800, 298
-        uref, sref, M, RU = 6885, 213.685, 44.01, ℙ(8.31447)
-        pars = ℙ.((M, Tmin, Tmax, Tref, uref, sref, RU))
-        @test SpecificHeat(ID, CP, pars..., B) isa SpecificHeat{ℙ}
+        for 𝔽 in (ℙ, float)
+            ID, B = :CO2_CB7_test, :MO
+            CP = T -> 𝔽(22.26 + 5.891e-2 * T - 3.501e-5 * T^2 + 7.469e-9 * T^3)
+            Tmin, Tref, Tmax  = 273, 298, 1800
+            uref, sref, M, RU = 6885, 213.685, 44.01, ℙ(8.31447)
+            pars = ℙ.((M, Tmin, Tref, Tmax, uref, sref, RU))
+            @test SpecificHeat(ID, CP, pars..., B) isa SpecificHeat{ℙ}
+        end
     end
 end
 
-@testset "cpModel.test.jl: inner constructor validations                            " begin
-    ID, B = :CO2_CB7_test, :MO
-    CP = T -> ℙ(22.26 + 5.891e-2 * T - 3.501e-5 * T^2 + 7.469e-9 * T^3)
-    Tmin, Tmax, Tref  = 273, 1800, 298
-    uref, sref, M, RU = 6885, 213.685, 44.01, 8.31447
+# Adjacent swaps of a tuple
+adjswp(t::Tuple) = [
+    ntuple(i -> i == j ? t[j+1] : i == j+1 ? t[j] : t[i], length(t)) for j in 1:length(t)-1
+]
 
+@testset "cpModel.test.jl: inner constructor validations                            " begin
+    for ℙ in union2vec(Base.IEEEFloat)
+        ID, B = :CO2_CB7_test, :MO
+        CP = T -> 22.26 + 5.891e-2 * T - 3.501e-5 * T^2 + 7.469e-9 * T^3
+        Tneg, Tmin, Tref, Tmax = -1, 273, 298, 1800
+        uref, sref, M, RU = 6885, 213.685, 44.01, 8.31447
+        pars = ℙ.((M, Tmin, Tref, Tmax, uref, sref, RU))
+        @test_throws "Error: Empty ID" SpecificHeat(Symbol(""), CP, pars..., B)
+        pars = ℙ.((0, Tmin, Tref, Tmax, uref, sref, RU))
+        @test_throws "Error: M <= 0" SpecificHeat(ID, CP, pars..., B)
+        pars = ℙ.((-M, Tmin, Tref, Tmax, uref, sref, RU))
+        @test_throws "Error: M <= 0" SpecificHeat(ID, CP, pars..., B)
+        for temp in adjswp(ℙ.((Tneg, Tmin, Tref, Tmax)))
+            pars = (ID, CP, ℙ.((M, temp[2:end]..., uref, sref, RU))..., B)
+            @test_throws "Error: Temperature values" SpecificHeat(pars...)
+        end
+    end
 end
 
 
