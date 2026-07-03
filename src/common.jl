f3d0e5fd7f93f98332da20c0326939c2fe6c193d
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-07-03 20:59:25 -0300

    UPD the src/common.jl

--- src/common.jl
+++ src/common.jl
@@ -12,3 +12,14 @@ FLOAT = Base.IEEEFloat
 
 # Chained Precision Composition Simplification
 ⊚(p::Type{ℙ}, c::ComposedFunction{Type{ℚ}}) where {ℙ <: FLOAT, ℚ <: FLOAT} = ⊚(p, c.inner)
+
+# Auxiliary methods
+function subscript(x::Int)
+    asSub(c::Char) = Char(Int(c) - Int('0') + Int('₀'))
+    map(asSub, "$(x)")
+end
+
+pDeco(::Type{Float16}) = subscript(16)
+pDeco(::Type{Float32}) = subscript(32)
+pDeco(::Type{Float64}) = subscript(64)
+
