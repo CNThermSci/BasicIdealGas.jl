056162445e7ba096a4d89f07507cd0e95210e65f
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-07-03 15:34:14 -0300

    UPD the src/cpModel.jl

--- src/cpModel.jl
+++ src/cpModel.jl
@@ -1,17 +1,5 @@
 # cpModel.jl - General Specific Heat Model
 
-# Type aliasing
-# -------------
-
-# IEEE-754 normalized floating point types of half, single, and double precision
-FLOAT = Base.IEEEFloat
-
-# Precision Composition Simplification
-⊚(p::Type{ℙ}, f::Function) where ℙ <: FLOAT = f(1) isa ℙ ? f : p ∘ f
-
-# Chained Precision Composition Simplification
-⊚(p::Type{ℙ}, c::ComposedFunction{Type{ℚ}}) where {ℙ <: FLOAT, ℚ <: FLOAT} = ⊚(p, c.inner)
-
 # Structure (type) definition
 # ---------------------------
 
