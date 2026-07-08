4cee54dd5de192b028ef9626336730f9310d4853
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-07-07 23:06:54 -0300

    UPD All 84 tests passing

--- test/cpModel.test.jl
+++ test/cpModel.test.jl
@@ -15,11 +15,11 @@ end
     for ℙ in union2vec(Base.IEEEFloat)
         for 𝔽 in (ℙ, float)
             B = :MO
-            FN = T -> 𝔽(22.26 + 5.891e-2 * T - 3.501e-5 * T^2 + 7.469e-9 * T^3)
+            𝑓 = T -> 𝔽(22.26 + 5.891e-2 * T - 3.501e-5 * T^2 + 7.469e-9 * T^3)
             Tmin, Tref, Tmax  = 273, 298, 1800
             uref, sref, MM, RU = 6885, 213.685, 44.01, ℙ(8.31447)
             pars = ℙ.((MM, Tmin, Tref, Tmax, uref, sref, RU))
-            @test SpecificHeat(FN, pars..., B) isa SpecificHeat{ℙ}
+            @test SpecificHeat(𝑓, pars..., B) isa SpecificHeat{ℙ}
         end
     end
 end
@@ -32,22 +32,22 @@ adjswp(t::Tuple) = [
 @testset "cpModel.test.jl: inner constructor validations                            " begin
     for ℙ in union2vec(Base.IEEEFloat)
         B = :MO
-        FN = T -> 22.26 + 5.891e-2 * T - 3.501e-5 * T^2 + 7.469e-9 * T^3
+        𝑓 = T -> 22.26 + 5.891e-2 * T - 3.501e-5 * T^2 + 7.469e-9 * T^3
         Tneg, Tmin, Tref, Tmax = -1, 273, 298, 1800
         uref, sref, MM, RU = 6885, 213.685, 44.01, 8.31447
         pars = ℙ.((0, Tmin, Tref, Tmax, uref, sref, RU))
-        @test_throws "Error: M <= 0" SpecificHeat(FN, pars..., B)
+        @test_throws "Error: M <= 0" SpecificHeat(𝑓, pars..., B)
         pars = ℙ.((-MM, Tmin, Tref, Tmax, uref, sref, RU))
-        @test_throws "Error: M <= 0" SpecificHeat(FN, pars..., B)
+        @test_throws "Error: M <= 0" SpecificHeat(𝑓, pars..., B)
         for temp in adjswp(ℙ.((Tneg, Tmin, Tref, Tmax)))
-            pars = (FN, ℙ.((MM, temp[2:end]..., uref, sref, RU))..., B)
+            pars = (𝑓, ℙ.((MM, temp[2:end]..., uref, sref, RU))..., B)
             @test_throws "Error: Temperature values" SpecificHeat(pars...)
         end
         pars = ℙ.((MM, Tmin, Tref, Tmax, uref, sref, 0))
-        @test_throws "Error: RU <= 0" SpecificHeat(FN, pars..., B)
+        @test_throws "Error: RU <= 0" SpecificHeat(𝑓, pars..., B)
         pars = ℙ.((MM, Tmin, Tref, Tmax, uref, sref, -RU))
-        @test_throws "Error: RU <= 0" SpecificHeat(FN, pars..., B)
-        pars = (FN, ℙ.((MM, Tmin, Tref, Tmax, uref, sref, RU))...)
+        @test_throws "Error: RU <= 0" SpecificHeat(𝑓, pars..., B)
+        pars = (𝑓, ℙ.((MM, Tmin, Tref, Tmax, uref, sref, RU))...)
         for b in (:ma, :mo, :other, Symbol(""))
             @test_throws "Error: B should be either :MA or :MO" SpecificHeat(pars..., b)
         end
@@ -56,38 +56,38 @@ end
 
 @testset "cpModel.test.jl: outer constructor return types                           " begin
     B = :MO
-    FN = T -> 22.26 + 5.891e-2 * T - 3.501e-5 * T^2 + 7.469e-9 * T^3
+    𝑓 = T -> 22.26 + 5.891e-2 * T - 3.501e-5 * T^2 + 7.469e-9 * T^3
     Tmin, Tref, Tmax = 273, 298, 1800
     uref, sref, MM, RU = 6885, 213685//1000, BigFloat("44.01"), π
     # Set type conversion / 1 indirection
     for ℙ in union2vec(Base.IEEEFloat)
-        pars = (FN, MM, Tmin, Tref, Tmax, uref, sref, RU, B)
+        pars = (𝑓, MM, Tmin, Tref, Tmax, uref, sref, RU, B)
         @test SpecificHeat{ℙ}(pars...) isa SpecificHeat{ℙ}
     end
     # Set type with unit conversion and stripping / 2 indirections
     for ℙ in union2vec(Base.IEEEFloat)
-        pars = (FN, MM*u"kg/kmol", Tmin, Tref, Tmax, uref*u"kJ/kmol", sref*u"kJ/kmol/K")
+        pars = (𝑓, MM*u"kg/kmol", Tmin, Tref, Tmax, uref*u"kJ/kmol", sref*u"kJ/kmol/K")
         @test SpecificHeat{ℙ}(pars..., RU) isa SpecificHeat{ℙ}
     end
     uref, sref, MM, RU = 6885, 213685//1000, 4401//100, 8.31447
     # Promotion type conversion / 2 indirections
     for ℙ in union2vec(Base.IEEEFloat)
-        pars = (FN, MM, Tmin, Tref, Tmax, uref, sref, ℙ(RU), B)
+        pars = (𝑓, MM, Tmin, Tref, Tmax, uref, sref, ℙ(RU), B)
         @test SpecificHeat(pars...) isa SpecificHeat{ℙ}
     end
     # Promotion type with unit conversion and stripping / 3 indirections
     for ℙ in union2vec(Base.IEEEFloat)
-        pars = (FN, MM*u"kg/kmol", Tmin, Tref, Tmax, uref*u"kJ/kmol", sref*u"kJ/kmol/K")
+        pars = (𝑓, MM*u"kg/kmol", Tmin, Tref, Tmax, uref*u"kJ/kmol", sref*u"kJ/kmol/K")
         @test SpecificHeat(pars..., ℙ(RU)) isa SpecificHeat{ℙ}
     end
 end
 
 @testset "cpModel.test.jl: type conversions                                         " begin
     B = :MO
-    FN = T -> 22.26 + 5.891e-2 * T - 3.501e-5 * T^2 + 7.469e-9 * T^3
+    𝑓 = T -> 22.26 + 5.891e-2 * T - 3.501e-5 * T^2 + 7.469e-9 * T^3
     Tmin, Tref, Tmax = 273.0, 298.0, 1800.0
     uref, sref, MM, RU = 6885.0, 213.685, 44.01, 8.31447
-    pars = (FN, MM, Tmin, Tref, Tmax, uref, sref, RU, B)
+    pars = (𝑓, MM, Tmin, Tref, Tmax, uref, sref, RU, B)
     SH = Dict(
         Float16 => SpecificHeat{Float16}(pars...),
         Float32 => SpecificHeat{Float32}(pars...),
@@ -96,8 +96,8 @@ end
     # Conversions
     for orig in union2vec(Base.IEEEFloat)
         for dest in union2vec(Base.IEEEFloat)
-            # Lossless FN conversion
-            @test SH[orig].FN === orig(SH[dest]).FN
+            # Lossless 𝑓 conversion
+            @test SH[orig].𝑓 === orig(SH[dest]).𝑓
             # Type conversion
             @test typeof(SH[orig]) === typeof(orig(SH[dest]))
         end
@@ -113,10 +113,10 @@ end
 
 @testset "cpModel.test.jl: type promotions                                          " begin
     B = :MO
-    FN = T -> 22.26 + 5.891e-2 * T - 3.501e-5 * T^2 + 7.469e-9 * T^3
+    𝑓 = T -> 22.26 + 5.891e-2 * T - 3.501e-5 * T^2 + 7.469e-9 * T^3
     Tmin, Tref, Tmax = 273.0, 298.0, 1800.0
     uref, sref, MM, RU = 6885.0, 213.685, 44.01, 8.31447
-    pars = (FN, MM, Tmin, Tref, Tmax, uref, sref, RU, B)
+    pars = (𝑓, MM, Tmin, Tref, Tmax, uref, sref, RU, B)
     SH = Dict(
         Float16 => SpecificHeat{Float16}(pars...),
         Float32 => SpecificHeat{Float32}(pars...),
