60bc804cbf790a1df4a03ff1d8e8bada9d6ef148
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-07-14 00:14:57 -0300

    UPD the src/idealState.jl

--- src/idealState.jl
+++ src/idealState.jl
@@ -22,48 +22,60 @@ end
 
 import Base: getproperty, propertynames
 
-function Base.getproperty(st::IdealState, s::Symbol)
-    if s in (:𝐺, :𝑃, :𝑇)
-        return getfield(st, s)
-    elseif s == :gas
+function Base.getproperty(st::IdealState, sy::Symbol)
+    if sy in (:𝐺, :𝑃, :𝑇)
+        return getfield(st, sy)
+    elseif sy == :gas
         return getfield(st, :𝐺)
-    elseif s == :P
+    elseif sy == :M
+        return getfield(st, :𝐺).hmod.𝑀 * u"kg/kmol"
+    elseif s in (:RMO, :RU, :Ru)
+        return getfield(st, :𝐺).hmod.𝑅 * u"kJ/kmol/K"
+    elseif sy == :P
         return getfield(st, :𝑃) * u"kPa"
-    elseif s == :T
+    elseif sy == :T
         return getfield(st, :𝑇) * u"K"
     end
-    GAS, P, T = map(fi -> getfield(st, fi), (:𝐺, :𝑃, :𝑇))
-    if s == :v
+    GAS, P, T = map(sy -> getfield(st, sy), (:𝐺, :𝑃, :𝑇))
+    if sy == :R
+        return R(GAS, :MA) * u"kJ/kg/K"
+    elseif s in (:γ, :ga, :gamma)
+        return ga(GAS, T)
+    elseif sy == :v
         return _v(GAS, P, T, :MA) * u"m^3/kg"
-    elseif s == :vMO
+    elseif sy == :vMO
         return _v(GAS, P, T, :MO) * u"m^3/kmol"
-    elseif s == :ρ
+    elseif sy == :ρ
         return _ρ(GAS, P, T, :MA) * u"kg/m^3"
-    elseif s == :ρMO
+    elseif sy == :ρMO
         return _ρ(GAS, P, T, :MO) * u"kmol/m^3"
-    elseif s == :cp
+    elseif sy == :cp
         return cp(GAS, T, :MA) * u"kJ/kg/K"
-    elseif s == :cpMO
+    elseif sy == :cpMO
         return cp(GAS, T, :MO) * u"kJ/kmol/K"
-    elseif s == :cv
+    elseif sy == :cv
         return cv(GAS, T, :MA) * u"kJ/kg/K"
-    elseif s == :cvMO
+    elseif sy == :cvMO
         return cv(GAS, T, :MO) * u"kJ/kmol/K"
-    elseif s == :u
+    elseif sy == :u
         return u(GAS, T, :MA) * u"kJ/kg"
-    elseif s == :uMO
+    elseif sy == :uMO
         return u(GAS, T, :MO) * u"kJ/kmol"
-    elseif s == :h
+    elseif sy == :h
         return h(GAS, T, :MA) * u"kJ/kg"
-    elseif s == :hMO
+    elseif sy == :hMO
         return h(GAS, T, :MO) * u"kJ/kmol"
+    elseif sy == :s0
+        return s0(GAS, T, :MA) * u"kJ/kg/K"
+    elseif sy == :s0MO
+        return s0(GAS, T, :MO) * u"kJ/kmol/K"
     end
 end
 
 Base.propertynames(::IdealState) = (
-    :gas, :P, :T,
+    :gas, :M, :R, :Ru, :RU, :P, :T,
     :v, :vMO, :ρ, :ρMO, :cp, :cpMO, :cv, :cvMO,
-    :u, :uMO, :h, :hMO, :s0, 
+    :u, :uMO, :h, :hMO, :s0, :s0MO,
 )
 
 # Export
