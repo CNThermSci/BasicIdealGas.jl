5ffb1fecd7b7377eccebc3dff61ff436b649b31d
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-07-15 19:24:59 -0300

    UPD the src/idealProcs.jl

--- src/idealProcs.jl
+++ src/idealProcs.jl
@@ -2,17 +2,41 @@
 
 function isoT(
         FR::IdealState{ℙ};
-        P::Union{Missing, Real} = missing,
-        v::Union{Missing, Real} = missing,
+        P::Union{
+            Missing,
+            Quantity{<:Real, dimension(u"kPa")},
+        } = missing,
+        v::Union{
+            Missing,
+            Quantity{<:Real, dimension(u"m^3/kg")},
+            Quantity{<:Real, dimension(u"m^3/kmol")},
+        } = missing,
+        s::Union{
+            Missing,
+            Quantity{<:Real, dimension(u"kJ/kg/K")},
+            Quantity{<:Real, dimension(u"kJ/kmol/K")},
+        } = missing,
     ) where {ℙ}
     @assert(
-        count(x -> isa(x, Real), (P, v)) == 1,
+        count(x -> !isa(x, Missing), (P, v)) == 1,
         "exactly one P-T-v state function must be specified!"
     )
     return if !ismissing(P)
-        IdealState{ℙ}(FR.gas, P, FR.𝑇)
+        IdealState{ℙ}(FR.gas, uconvert(u"kPa", P).val, FR.𝑇)
     elseif !ismissing(v)
-        IdealState{ℙ}(FR.gas, _P(FR.gas, FR.𝑇, v, :MA), FR.𝑇)
+        if dimension(v) == dimension(u"m^3/kg")
+            IdealState{ℙ}(
+                FR.gas,
+                _P(FR.gas, FR.𝑇, uconvert(u"m^3/kg", v).val, :MA),
+                FR.𝑇
+            )
+        else
+            IdealState{ℙ}(
+                FR.gas,
+                _P(FR.gas, FR.𝑇, uconvert(u"m^3/kmol", v).val, :MO),
+                FR.𝑇
+            )
+        end
     end
 end
 
