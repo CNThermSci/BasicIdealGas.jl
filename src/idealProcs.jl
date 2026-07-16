5a981431823d6e5d3597366a4160ba95715825d0
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-07-16 17:05:24 -0300

    UPD the src/idealProcs.jl

--- src/idealProcs.jl
+++ src/idealProcs.jl
@@ -39,3 +39,41 @@ function isoT(
 end
 
 export isoT
+
+function isoP(
+        FR::IdealState{ℙ};
+        T::Union{
+            Missing,
+            Quantity{<:Real, dimension(u"K")},
+        } = missing,
+        v::Union{
+            Missing,
+            Quantity{<:Real, dimension(u"m^3/kg")},
+            Quantity{<:Real, dimension(u"m^3/kmol")},
+        } = missing,
+        s::Union{
+            Missing,
+            Quantity{<:Real, dimension(u"kJ/kg/K")},
+            Quantity{<:Real, dimension(u"kJ/kmol/K")},
+        } = missing,
+    ) where {ℙ}
+    @assert(
+        count(x -> !isa(x, Missing), (T, v, s)) == 1,
+        "exactly one end-state function must be specified!"
+    )
+    return if !ismissing(T)
+        FR(T = uconvert(u"K", ℙ(T)).val)
+    elseif !ismissing(v)
+        if dimension(v) == dimension(u"m^3/kg")
+            FR(T = _T(FR.gas, FR.𝑃, uconvert(u"m^3/kg", ℙ(v)).val, :MA))
+        else
+            FR(T = _T(FR.gas, FR.𝑃, uconvert(u"m^3/kmol", ℙ(v)).val, :MO))
+        end
+    elseif !ismissing(s)
+        if dimension(s) == dimension(u"kJ/kg/K")
+        else
+        end
+    end
+end
+
+export isoP
