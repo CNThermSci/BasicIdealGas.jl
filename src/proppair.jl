cc2518568f9ac4bcf9f81515b55a86f4908161e5
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-07-23 23:38:51 -0300

    UPD the src/proppair.jl

--- src/proppair.jl
+++ src/proppair.jl
@@ -4,4 +4,15 @@
 # ---------------------------
 
 struct PropPair{ℙ <: FLOAT}
+    𝑃::ℙ                # kPa
+    𝑇::ℙ                # K
+    # Internal, validating constructors
+    function PropPair(
+            P::ℙ,
+            T::ℙ,
+        ) where {ℙ <: FLOAT}
+        @assert(P > zero(ℙ), "P = $(P) ⩽ 0")
+        @assert(T > zero(ℙ), "T = $(T) ⩽ 0")
+        return new{ℙ}(P, T)
+    end
 end
