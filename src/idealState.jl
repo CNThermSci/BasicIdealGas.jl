4f575608be9b581c8a58eab2cd5e29d8542e7d69
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-08-14 20:03:59 -0300

    UPD the src/idealState.jl

--- src/idealState.jl
+++ src/idealState.jl
@@ -88,7 +88,7 @@ function (ξ::IdealState{ℙ})(
     else
         # copy-edit variant
         if ismissing(p)
-            IdealState{ℙ}(ξ.𝐺, PropPair{ℙ}(P, T))
+            IdealState{ℙ}(ξ.𝐺, ξ.𝑝(P, T))
         else
             IdealState{ℙ}(ξ.𝐺, p(P = P, T = T))
         end
