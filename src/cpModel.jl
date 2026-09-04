03dfb6d7efadff3b934268eafdbe257de4dfb1e9
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-09-03 22:56:00 -0300

    UPD the src/cpModel.jl

--- src/cpModel.jl
+++ src/cpModel.jl
@@ -118,10 +118,7 @@ export SpecificHeat
 
 function Base.show(io::IO, ::MIME"text/plain", ξ::SpecificHeat{ℙ}) where {ℙ <: FLOAT}
     # rng = "[$(@sprintf("%.*g K", 5, ξ.Tmin)) $(@sprintf("%.*g K", 5, ξ.Tmax))]"
-    return print(
-        io,
-        "$(ξ.ID) cp$(pDeco(ℙ))(T)"
-    )
+    return print(io, "$(ξ.ID) cp$(pDeco(ℙ))(T)")
 end
 
 # SpecificHeat Helper functions
