8825b494589b0224ebc1f7ce8d0356f99103ffef
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-09-03 21:57:22 -0300

    UPD the src/common.jl

--- src/common.jl
+++ src/common.jl
@@ -39,15 +39,6 @@ const MOLR = Union{
     Quantity{ℙ, dimension(u"kmol/m^3")},
 } where {ℙ <: Real}
 
-# Thermodynamic state function Quantity type alias - unit set (for type defs)
-const PRESty{ℙ <: FLOAT} = Quantity{ℙ, dimension(u"kPa"), typeof(u"kPa")}
-const TEMPty{ℙ <: FLOAT} = Quantity{ℙ, dimension(u"K"), typeof(u"K")}
-const VOLUty{ℙ <: FLOAT} = Quantity{ℙ, dimension(u"m^3/kmol"), typeof(u"m^3/kmol")}
-const MOLWty{ℙ <: FLOAT} = Quantity{ℙ, dimension(u"kg/kmol"), typeof(u"kg/kmol")}
-const ENERty{ℙ <: FLOAT} = Quantity{ℙ, dimension(u"kJ/kmol"), typeof(u"kJ/kmol")}
-const ENTRty{ℙ <: FLOAT} = Quantity{ℙ, dimension(u"kJ/kmol/K"), typeof(u"kJ/kmol/K")}
-const DENSty{ℙ <: FLOAT} = Quantity{ℙ, dimension(u"kmol/m^3"), typeof(u"kmol/m^3")}
-
 # Thermodynamic unit conversion/stripping
 kSI(x::Real) = x
 kSI(x::PRES) = uconvert(u"kPa", x).val
