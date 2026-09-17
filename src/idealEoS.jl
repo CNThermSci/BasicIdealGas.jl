874220003dcbbd4def0c80e89e289c3e81294898
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-09-17 12:55:17 -0300

    UPD the src/idealEoS.jl

--- src/idealEoS.jl
+++ src/idealEoS.jl
@@ -108,7 +108,7 @@ end
 # TODO: feat/units below
 
 # Internal, fast, positional, UNITFUL, EoS functions
-_P(ξ::IdealGas{ℙ}, T::Real, v::Real, B::Symbol = :MA) where {ℙ} = R(ξ, B) * ℙ(T / v)
+_P(ξ::IdealGas{ℙ}, 𝑇::TEMP, 𝑣::VOLU, B::Symbol = :MA) where {ℙ} = R(ξ, B) * ℙ(𝑇 / 𝑣)
 _T(ξ::IdealGas{ℙ}, P::Real, v::Real, B::Symbol = :MA) where {ℙ} = ℙ(P * v) / R(ξ, B)
 _v(ξ::IdealGas{ℙ}, P::Real, T::Real, B::Symbol = :MA) where {ℙ} = R(ξ, B) * ℙ(T / P)
 _ρ(ξ::IdealGas{ℙ}, P::Real, T::Real, B::Symbol = :MA) where {ℙ} = inv(_v(ξ, P, T, B))
